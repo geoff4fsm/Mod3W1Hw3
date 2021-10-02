22bbc3d11d4955a723193d0137b4faaf82c30417
geoff4fsm <87156765+geoff4fsm@users.noreply.github.com>
2021-10-02 12:49:34 -0400

    Add files via upload

--- Homework/Calculator.java
+++ Homework/Calculator.java
@@ -3,41 +3,33 @@ import java.util.Scanner;
 
 public class Calculator {
 	
-	public static void add(double num1, double num2) {
-
-			double sum = num1 + num2 ;
-			System.out.println(num1 + " + " + num2 + " = " + sum);
-
+	public static double add(double num1, double num2, double sumNums) {
+		sumNums = num1 + num2;
+		return  sumNums;
 	}
 	
-	public static void subtract(double num1, double num2) {
+	public static double subtract(double num1, double num2, double differenceNums) {
 
-			double difference = num1 - num2;
-			System.out.println(num1 + " - " + num2 + " = " + difference);
-
+		differenceNums = num1 - num2;
+		return differenceNums;
 	}
 	
-	public static void multiply(double num1, double num2) {
+	public static double multiply(double num1, double num2, double productNums) {
 			
-			double product = num1 * num2;
-			System.out.println(num1 + " * " + num2 + " = " + product);
-
+		productNums = num1 * num2;
+		return productNums;
 	}
 	
-	public static void divide(double num1, double num2) {
+	public static double divide(double num1, double num2, double quotientNums) {
 			
-			if ( num2 != 0 ) {
-				double quotient = num1 / num2;
-				System.out.println(num1 + " / " + num2 + " = " + quotient);
-			}
-			else System.out.println("cannot divide by 0");	
-
+		quotientNums = num1 / num2;
+		return quotientNums;	
 	}
 	
 	public static void main(String[] args) {
 		
 		char operator = ' ';
-		double x, y;
+		double x, y, sum, difference, product, quotient;
 		
 		while (operator != '#') {
 			
@@ -55,8 +47,9 @@ public class Calculator {
 						System.out.println("Enter numbers to be added ");					
 							x = input.nextDouble();
 							y = input.nextDouble();
-							
-						add(x, y);
+							sum = 0 ;							
+								add(x, y, sum);					
+								System.out.println( x + " + " + y + " = " + add(x, y, sum) );
 						break;
 				
 					case '-' :
@@ -64,28 +57,36 @@ public class Calculator {
 						System.out.println("Enter numbers to be subtracted ");					
 							x = input.nextDouble();
 							y = input.nextDouble();
-							
-						subtract(x, y);
+							difference = 0;							
+								subtract(x, y, difference);
+								System.out.println( x + " - " + y + " = " + subtract(x, y, difference) );
 						break;
 				
 					case '*' :
 						
 						System.out.println("Enter numbers to be multiplied ");					
-						x = input.nextDouble();
-						y = input.nextDouble();
-						multiply(x, y);
+							x = input.nextDouble();
+							y = input.nextDouble();
+							product = 0;
+								multiply(x, y, product);
+								System.out.println( x + " * " + y + " = " + multiply(x, y, product) );
 						break;
 				
 					case '/' :
 						
 						System.out.println("Enter numbers to be divided ");					
-						x = input.nextDouble();
-						y = input.nextDouble();
-						divide(x, y);
+							x = input.nextDouble();
+							y = input.nextDouble();
+							quotient = 0;
+								if (y != 0) {
+									divide(x, y, quotient);
+									System.out.println( x + " / " + y + " = " + divide(x, y, quotient) );
+								} 
+								else System.out.println("Cannot divide by 0, Try again ");
 						break;
 				
 					default :
-						System.out.println("undefined operator, try again");
+						System.out.println("Undefined operator, Try again");
 				}
 		}					
 	}
